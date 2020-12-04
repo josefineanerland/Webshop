d6d54b73089413b328727e07b92b53cfd5427562
Josefine Anerland <josefineanerland@gmail.com>
2020-12-04 23:36:12 +0100

    Added more products, productcontroller and views for products

--- Webshop/Data/ApplicationDbContext.cs
+++ Webshop/Data/ApplicationDbContext.cs
@@ -37,8 +37,28 @@ namespace Webshop.Data
                     Name = "Falselash F100",
                     Price = 60,
                     Quantity = 10,
-                    ImageUrl = @" ",
+                    ImageUrl = @"..\Webshop\Webshop\Images\MinklashF100.jpg",
                     Description = "Falselash made of faux mink, cruelty free."
+                },
+
+                new Models.Product()
+                {
+                    Id = 2,
+                    Name = "Longlash F200",
+                    Price = 70,
+                    Quantity = 15,
+                    ImageUrl = @"..\Webshop\Webshop\Images\LonglashF200.jpg",
+                    Description = "Falselash made of faux mink, cruelty free."
+                },
+                
+                new Models.Product()
+                {
+                    Id = 3,
+                    Name = "Double pack of long falselashes",
+                    Price = 150,
+                    Quantity = 20,
+                    ImageUrl = @"..\Webshop\Webshop\Images\MinklashDouble.jpg",
+                    Description = "Double pack of falsies, made of faux mink. Cruelty free."
                 });
 
         }
---
+++ Webshop/Data/Migrations/20201204222535_AddedMoreProducts.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Webshop.Data.Migrations
+{
+    public partial class AddedMoreProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "..\\Webshop\\Webshop\\Images\\MinklashF100.jpg");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price", "Quantity" },
+                values: new object[] { 3, "Double pack of falsies, made of faux mink. Cruelty free.", "..\\Webshop\\Webshop\\Images\\MinklashDouble.jpg", "Double pack of long falselashes", 150.0, 20 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price", "Quantity" },
+                values: new object[] { 2, "Falselash made of faux mink, cruelty free.", "..\\Webshop\\Webshop\\Images\\LonglashF200.jpg", "Longlash F200", 70.0, 15 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: " ");
+        }
+    }
+}
